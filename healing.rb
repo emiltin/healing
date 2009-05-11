8e00c6180ad50748ed1f02d91af4bb4d2bb52ef4
Emil Tin <emil@tin.dk>
2009-05-11 12:35:14 +0200

    RemoteHealing class

--- healing.rb
+++ healing.rb
@@ -6,6 +6,7 @@ require 'resources/cloud.rb'
 require 'providers/ec2_provider.rb'
 require 'nodes/node.rb'
 require 'local.rb'
+require 'remote.rb'
 
 
 class Healing
@@ -20,43 +21,6 @@ class Healing
     require 'ideal.rb'
     @cloud = Healing::Cloud.root
     raise "No cloud defined in ideal.rb!" unless @cloud
-  end
-  
-  def load_addresses
-    @addr = "ec2-174-129-95-238.compute-1.amazonaws.com"    #should get from cloud provider (ec2)
-  end
-  
-  def describe
-    load
-    @cloud.describe :recurse => true
-  end
-  
-  def install
-    puts 'installing'
-    #it seems ssh here doesn't work if we use ~ in the path?
-    Healing.run_locally "rsync -e 'ssh -i #{@cloud.key}' -ar /Users/emiltin/Desktop/healing/ root@#{@addr}:/healing"
-  end
-  
-  def set_cloud_uuid
-    puts 'setting cloud uuid'
-    Healing.run_locally "ssh -i #{@cloud.key} root@#{@addr} \"echo '#{@cloud.uuid}' > /healing/cloud_uuid\""
-  end
-  
-  
-  def start
-    load
-    load_addresses
-    install
-    set_cloud_uuid
-    heal
-  end
-  
-  def heal
-    load
-    load_addresses
-    puts "\nHealing node #{@addr}."
-    install   #should only upload ideal.rb and recipes...
-    Healing.run_locally "ssh -i #{@cloud.key} root@#{@addr} \"cd /healing && bin/heal-local\""
   end
 
 end
